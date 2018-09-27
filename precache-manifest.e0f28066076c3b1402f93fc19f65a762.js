8f1f61f61247061327d1e8c5aa12db22978e97d8
hzbdesigner <hzbdesigner@gmail.com>
2018-09-27 16:47:48 +0800

    Updates

---
+++ precache-manifest.e0f28066076c3b1402f93fc19f65a762.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eda29f6f625ebae01331",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "eda29f6f625ebae01331",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "44dd3333d0ce70df3163ee28484ec397",
+    "url": "/static/yay.44dd3333.jpg"
+  },
+  {
+    "revision": "1521fc7a683c79276d8d34b8e9f53d21",
+    "url": "/static/title-bg.1521fc7a.png"
+  },
+  {
+    "revision": "1bfcc7276a53f3aef3a534114267f7ef",
+    "url": "/static/banner.1bfcc727.jpg"
+  }
+];
